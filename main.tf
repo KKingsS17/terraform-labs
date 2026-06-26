3d8045a4e521b0e3e489a53f034fa6a63673c0e6
KKingsS17 <kendallrp1706@gmail.com>
2026-06-26 16:30:00 -0600

    Added locals

--- main.tf
+++ main.tf
@@ -13,12 +13,23 @@ provider "azurerm" {
   features {}
 }
 
-# Create the very first resource
-resource "azurerm_resource_group" "contoso_rg" {
-  name     = "contoso_rg"
-  location = "UK South"
-
+locals {
+  prefix = "contoso"
+  region = "UK South"
   tags = {
     cost_center = "contoso research"
   }
+}
+
+# Create the very first resource
+resource "azurerm_resource_group" "contoso_rg" {
+  name     = "${local.prefix}_rg"
+  location = local.region
+  tags     = local.tags
+}
+
+resource "azurerm_resource_group" "contoso_dev_rg" {
+  name     = "${local.prefix}_dev_rg"
+  location = local.region
+  tags     = local.tags
 }
