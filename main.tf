4bfebeb170293eaee408a6d80733a0bf37bec0df
KKingsS17 <kendallrp1706@gmail.com>
2026-06-27 11:20:24 -0600

    Worked with data sources and inputs

--- main.tf
+++ main.tf
@@ -13,6 +13,10 @@ provider "azurerm" {
   features {}
 }
 
+locals {
+  subnets = cidrsubnets("10.0.0.0/24", 6, 7, 8)
+}
+
 # Create the very first resource
 resource "azurerm_resource_group" "demo" {
   for_each = var.resource_groups
